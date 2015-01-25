47d41de65f67f8e9a618fb8a2c7c1567141f60bb
Gerard Maas <gerard.maas@virdata.com>
2015-01-25 23:57:40 +0100

    implements the cache and matrix inversion logic

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function provides a cache for matrix inversion
 
-## Write a short comment describing this function
-
+## Creates cache for the inverse of the provided matrix 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache <- NULL
+  set <- function(y) {
+    x <<- y
+    cache <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) cache <<- inv
+  getInverse <- function() cache
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Retrieves the inverse of the wrapped matrix. The inverse is memoized the first time it's accessed 
+## and this cache is further used while the base matric does not change
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cache <- x$getInverse()
+  if(!is.null(cache)) {
+    message("getting cached data")
+    return(cache)
+  }
+  data <- x$get()
+  cache <- solve(data, ...)
+  x$setInverse(cache)
+  cache
 }
